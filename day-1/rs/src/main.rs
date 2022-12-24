ff8267e9372b4b1a417e8954fb8742fac9702ea2
Juan de Bravo <juandebravo@gmail.com>
2022-12-24 17:09:55 +0100

    implement day1 - part2

--- day-1/rs/src/main.rs
+++ day-1/rs/src/main.rs
@@ -1,6 +1,6 @@
 use std::env;
 
-fn get_max(a: i32, b: i32) -> i32 {
+fn get_max(a: u32, b: u32) -> u32 {
     if a > b {
         a
     } else {
@@ -11,17 +11,18 @@ fn get_max(a: i32, b: i32) -> i32 {
 #[test]
 fn test_get_max() {
     assert_eq!(get_max(1, 2), 2);
-    assert_eq!(get_max(1, -2), 1);
+    assert_eq!(get_max(1, 0), 1);
 }
 
 fn get_data(cookie: String) -> Result<String, reqwest::Error> {
-    let url = "https://adventofcode.com/2022/day/1/input";
+    let url = "https://adventofcode.com/2022/day/1/input".to_string();
     let client = reqwest::blocking::Client::new();
+
     let resp = client.get(url).header("cookie", cookie).send();
     resp?.text()
 }
 
-fn part1(lines: &Vec<&str>) -> i32 {
+fn part1(lines: &Vec<&str>) -> u32 {
     let mut curr = 0;
     let mut max = 0;
 
@@ -29,7 +30,7 @@ fn part1(lines: &Vec<&str>) -> i32 {
         if line == "" {
             max = get_max(max, curr);
             curr = 0;
-        } else if let Ok(x) = String::from(line).parse::<i32>() {
+        } else if let Ok(x) = String::from(line).parse::<u32>() {
             curr += x;
         } else {
             panic!("Invalid number")
@@ -38,11 +39,35 @@ fn part1(lines: &Vec<&str>) -> i32 {
     max
 }
 
-fn part2(_lines: &[&str]) -> i32 {
-    0
+fn part2(lines: &[&str]) -> u32 {
+    let mut curr = 0;
+    let mut max = vec![0; 3];
+
+    for &line in lines {
+        if line == "" {
+            max[0] = get_max(max[0], curr);
+            max.sort();
+            curr = 0;
+        } else if let Ok(x) = String::from(line).parse::<u32>() {
+            curr += x;
+        } else {
+            panic!("Invalid number")
+        }
+    }
+    max.into_iter().sum()
+}
+
+struct ExpectedResult {
+    part1: u32,
+    part2: u32,
 }
 
 fn main() {
+    let expected_result = ExpectedResult {
+        part1: 71124,
+        part2: 204639,
+    };
+
     let args: Vec<String> = env::args().skip(1).collect();
     assert!(args.len() == 1);
 
@@ -52,8 +77,17 @@ fn main() {
     match data {
         Ok(value) => {
             let lines = value.split("\n").collect::<Vec<&str>>();
-            println!("{}", part1(&lines));
-            println!("{}", part2(&lines));
+
+            assert_eq!(part1(&lines), expected_result.part1);
+            assert_eq!(part2(&lines), expected_result.part2);
+
+            println!(
+                "{}",
+                format!(
+                    "Done! \n\tpart1: {}\n\tpart2: {}",
+                    expected_result.part1, expected_result.part2
+                )
+            );
         }
         Err(e) => panic!("{}", format!("Invalid data {:?}", e)),
     }
