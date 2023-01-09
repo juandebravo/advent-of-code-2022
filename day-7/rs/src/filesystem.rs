69e9392b4bc5871299d9021ac3d70c779f29f9fd
Juan de Bravo <juandebravo@gmail.com>
2023-01-09 22:57:49 +0100

    thanks https://applied-math-coding.medium.com/a-tree-structure-implemented-in-rust-8344783abd75!

--- day-7/rs/src/filesystem.rs
+++ day-7/rs/src/filesystem.rs
@@ -1,3 +1,4 @@
+use std::cell::RefCell;
 use std::collections::HashMap;
 use std::rc::Rc;
 
@@ -26,9 +27,9 @@ fn test_can_create_file() {
 #[derive(Debug, Clone)]
 pub struct Folder {
     pub name: String,
-    parent: Option<Rc<Folder>>,
-    pub folders: HashMap<String, Folder>,
-    pub files: HashMap<String, File>,
+    parent: Option<Rc<RefCell<Folder>>>,
+    pub folders: HashMap<String, Rc<RefCell<Folder>>>,
+    pub files: Vec<File>,
 }
 
 impl Folder {
@@ -37,44 +38,48 @@ impl Folder {
             name: name.to_string(),
             parent: None,
             folders: HashMap::new(),
-            files: HashMap::new(),
+            files: Vec::new(),
         }
     }
 
     pub fn size(&self) -> u32 {
         let mut total_size = 0;
-        for (_name, file) in &self.files {
+        for file in &self.files {
             total_size += file.size;
         }
         for (_name, folder) in &self.folders {
-            total_size += folder.size();
+            let f = folder.try_borrow().unwrap();
+            total_size += f.size();
         }
         total_size
     }
 
     pub fn add_file(&mut self, file: File) -> () {
-        self.files.insert(file.name.clone(), file);
+        self.files.push(file);
     }
 
-    pub fn add_folder(&mut self, mut folder: Folder) -> () {
-        folder.parent = Some(Rc::new(self.clone()));
-        self.folders.insert(folder.name.clone(), folder);
+    pub fn add_folder(&mut self, name: String, folder: Rc<RefCell<Folder>>) -> () {
+        self.folders.insert(name, folder);
     }
 
     pub fn path(&self) -> String {
-        let mut path = Vec::new();
-        let mut current = self;
-        while let Some(parent) = &current.parent {
-            path.push(current.name.as_str());
-            current = parent;
+        let path = self.name.clone();
+        match &self.parent {
+            None => format!("/{path}"),
+            Some(p) => {
+                let parent = p.borrow();
+                let mut _path = parent.path();
+                _path.push_str("/");
+                _path.push_str(&path);
+
+                // Make sure we avoid double slash
+                if _path[0..2].to_string() == "//" {
+                    _path[1..].to_string()
+                } else {
+                    _path
+                }
+            }
         }
-        if current.name.as_str() != "" {
-            path.push(current.name.as_str());
-        }
-        path.reverse();
-        let mut value = "/".to_string();
-        value.push_str(path.join("/").as_str());
-        value
     }
 }
 
@@ -88,13 +93,6 @@ fn test_can_create_folder() {
 }
 
 #[test]
-fn test_can_add_subfolder_to_folder() {
-    let mut root = Folder::new("root");
-    root.add_folder(Folder::new("foo"));
-    assert_eq!(root.folders.len(), 1);
-}
-
-#[test]
 fn test_can_add_files_to_folder() {
     let mut root = Folder::new("root");
     root.add_file(File::new("foo", 10));
@@ -105,30 +103,47 @@ fn test_can_add_files_to_folder() {
 
 #[test]
 fn test_check_total_folder_size() {
-    let mut root = Folder::new("root");
+    let root = Rc::new(RefCell::new(Folder::new("")));
+    let current = Rc::clone(&root);
 
-    let mut subfolder = Folder::new("foo");
-    subfolder.add_file(File::new("bazz", 40));
+    current.borrow_mut().add_file(File::new("foo", 10));
+    current.borrow_mut().add_file(File::new("bar", 20));
 
-    let mut subsubfolder = Folder::new("foo");
-    subsubfolder.add_file(File::new("one", 50));
-    subsubfolder.add_file(File::new("two", 60));
+    for (folder, files) in [
+        ("foo", vec![("bazz", 40)]),
+        ("bar", vec![("one", 50), ("two", 60)]),
+    ] {
+        let child = Rc::new(RefCell::new(Folder::new(folder)));
 
-    subfolder.add_folder(subsubfolder);
-    root.add_folder(subfolder);
+        current
+            .borrow_mut()
+            .add_folder(folder.to_string(), Rc::clone(&child));
 
-    root.add_file(File::new("foo", 10));
-    root.add_file(File::new("bar", 20));
+        let mut mut_child = child.borrow_mut();
+        mut_child.parent = Some(Rc::clone(&current));
 
-    assert_eq!(root.size(), 180);
-    assert_eq!(root.folders.get("foo").unwrap().size(), 150);
+        for (name, size) in files {
+            mut_child.add_file(File::new(name, size));
+        }
+    }
+
+    assert_eq!(root.borrow().size(), 180);
+
     assert_eq!(
-        root.folders
-            .get("foo")
-            .expect("Folder not found")
+        root.borrow()
             .folders
             .get("foo")
             .expect("Folder not found")
+            .borrow()
+            .size(),
+        40
+    );
+    assert_eq!(
+        root.borrow()
+            .folders
+            .get("bar")
+            .expect("Folder not found")
+            .borrow()
             .size(),
         110
     );
@@ -136,45 +151,63 @@ fn test_check_total_folder_size() {
 
 #[test]
 fn test_folder_path() {
-    let bazz = Folder::new("bazz");
+    let root = Rc::new(RefCell::new(Folder::new("")));
 
-    let mut bar = Folder::new("bar");
-    bar.add_folder(bazz);
+    let mut current = Rc::clone(&root);
 
-    let mut foo = Folder::new("foo");
-    foo.add_folder(bar);
+    for name in ["foo", "bar", "bazz"] {
+        let child = Rc::new(RefCell::new(Folder::new(name)));
 
-    let mut root = Folder::new("");
-    root.add_folder(foo);
+        current
+            .borrow_mut()
+            .add_folder(name.to_string(), Rc::clone(&child));
+        {
+            let mut mut_child = child.borrow_mut();
+            mut_child.parent = Some(Rc::clone(&current));
+        }
+        current = child;
+    }
 
-    assert_eq!(root.path(), "/");
+    assert_eq!(root.borrow().path(), "/");
 
     assert_eq!(
-        root.folders.get("foo").expect("Folder not found").path(),
+        root.borrow()
+            .folders
+            .get("foo")
+            .expect("Folder not found")
+            .borrow()
+            .path(),
         "/foo"
     );
 
     assert_eq!(
-        root.folders
+        root.borrow()
+            .folders
             .get("foo")
             .expect("Folder not found")
+            .borrow()
             .folders
             .get("bar")
             .expect("Folder not found")
+            .borrow()
             .path(),
         "/foo/bar"
     );
 
     assert_eq!(
-        root.folders
+        root.borrow()
+            .folders
             .get("foo")
             .expect("Folder not found")
+            .borrow()
             .folders
             .get("bar")
             .expect("Folder not found")
+            .borrow()
             .folders
             .get("bazz")
             .expect("Folder not found")
+            .borrow()
             .path(),
         "/foo/bar/bazz"
     );
